0fc7d6a39ba6196862e47e38f1d4729f888be849
zoltanf <zoltanf@gmail.com>
2026-03-13 16:32:30 +0100

    First cask commit

---
+++ Casks/mbox-viewer.rb
@@ -0,0 +1,12 @@
+cask "mbox-viewer" do
+  version "1.5.2603131546"
+  sha256 "49867d976d7c15e4dfd0685d65ad2cf33736fd5982b752e94809bda7127d9976"
+
+  url "https://github.com/zoltanf/MboxViewer/releases/download/v1.5.2603131546/Mbox.Viewer-1.5.2603131546-arm64-mac.dmg"
+  name "Mbox Viewer"
+  desc "Desktop viewer for mbox and PST email archives"
+  homepage "https://github.com/zoltanf/MboxViewer"
+
+  app "Mbox Viewer.app"
+end
+
