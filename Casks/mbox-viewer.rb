80db702d4546a4e0a503a522fe3f4e8e28563311
zoltanf <zoltanf@gmail.com>
2026-03-17 12:24:57 +0100

    Bump version for the latest one.

--- Casks/mbox-viewer.rb
+++ Casks/mbox-viewer.rb
@@ -1,8 +1,8 @@
 cask "mbox-viewer" do
   version "1.6.2603132210"
-  sha256 "f6bbcb6183938e8e7e30b755787b33945bafea38e19ae2dbef32fcd1ce58fa3d"
+  sha256 "e195ab362c936ff71ce0483fb9fb2afb51d77a5979874ae9dfacfc8a13bb0753"
 
-  url "https://github.com/zoltanf/MboxViewer/releases/download/v1.6.2603132210/Mbox.Viewer-1.6.2603132210-arm64-mac.dmg"
+  url "https://github.com/zoltanf/MboxViewer/releases/download/v1.7.2603171115/Mbox-Viewer-1.7.2603171115-arm64.dmg"
   name "Mbox Viewer"
   desc "Desktop viewer for mbox, pst and eml email archives"
   homepage "https://github.com/zoltanf/MboxViewer"
