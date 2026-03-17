a91b7c9850f741b48d4545c3fed0beec2d870e22
zoltanf <zoltanf@gmail.com>
2026-03-17 12:45:30 +0100

    Actually update the version as well :)

--- Casks/mbox-viewer.rb
+++ Casks/mbox-viewer.rb
@@ -1,5 +1,5 @@
 cask "mbox-viewer" do
-  version "1.6.2603132210"
+  version "1.7.2603171115"
   sha256 "e195ab362c936ff71ce0483fb9fb2afb51d77a5979874ae9dfacfc8a13bb0753"
 
   url "https://github.com/zoltanf/MboxViewer/releases/download/v1.7.2603171115/Mbox-Viewer-1.7.2603171115-arm64.dmg"
